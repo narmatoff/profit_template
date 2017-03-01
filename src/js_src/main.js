0dacd294e82a442cc3cf6443aa631aff5868fac1
Sergey <narmatoff@MacBook-Pro-Sergey.local>
2017-03-01 14:52:24 +0300

    added magnificPopup plugin for modal windows

--- src/js_src/main.js
+++ src/js_src/main.js
@@ -131,4 +131,69 @@ Pace.on('done', function() {
         $('.callback_block').toggleClass('callback_block_expand');
     });
 
+
+
+
+
+    // Модальные окна
+    /////////////////////////////////////////////////////////////////////////////////////////////////////
+    // Галерея картинок
+    $('.magnific_gallary').each(function() { // the containers for all your galleries
+        $(this).magnificPopup({
+            delegate: 'a', // the selector for gallery item
+            type: 'image',
+            gallery: {
+                enabled: true
+            },
+            mainClass: 'mfp-with-zoom', // this class is for CSS animation below
+
+            zoom: {
+                enabled: true, // By default it's false, so don't forget to enable it
+
+                duration: 300, // duration of the effect, in milliseconds
+                easing: 'ease-in-out', // CSS transition easing function
+
+                // The "opener" function should return the element from which popup will be zoomed in
+                // and to which popup will be scaled down
+                // By defailt it looks for an image tag:
+                opener: function(openerElement) {
+                    // openerElement is the element on which popup was initialized, in this case its <a> tag
+                    // you don't need to add "opener" option if this code matches your needs, it's defailt one.
+                    return openerElement.is('img') ? openerElement : openerElement.find('img');
+                }
+            }
+        });
+    });
+
+    // всплывающие картинки
+    $('.magnific_img').magnificPopup({
+        // delegate: 'a', // the selector for gallery item
+        type: 'image',
+        image: {
+            markup: '<div class="mfp-figure">' +
+                '<div class="mfp-close"></div>' +
+                '<div class="mfp-img"></div>' +
+                // '<div class="mfp-bottom-bar">' +
+                // '<div class="mfp-title"></div>' +
+                // '<div class="mfp-counter"></div>' +
+                '</div>' +
+                '</div>', // Popup HTML markup. `.mfp-img` div will be replaced with img tag, `.mfp-close` by close button
+
+            cursor: 'mfp-zoom-out-cur', // Class that adds zoom cursor, will be added to body. Set to null to disable zoom out cursor.
+
+            // titleSrc: 'title', // Attribute of the target element that contains caption for the slide.
+            // Or the function that should return the title. For example:
+            // titleSrc: function(item) {
+            //   return item.el.attr('title') + '<small>by Marsel Van Oosten</small>';
+            // }
+
+            verticalFit: true, // Fits image in area vertically
+
+            tError: '<a href="%url%">Изображение</a> не открывается.' // Error message
+        },
+        gallery: {
+            enabled: true
+        }
+    });
+
 });
